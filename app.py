bccfeb83a4c74be4efc61c8bcc48bddf034eb681
prianshu143 <110248829+prianshu143@users.noreply.github.com>
2022-08-20 03:00:29 +0530

    Update app.py
    Signed-off-by: prianshu143 <110248829+prianshu143@users.noreply.github.com>

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def predict():
   return render_template('index.html', prediction_text='NLP Model  has predicted about the text : {}'.format(result))
 
 
-app.run()
+if __name__=="__main__":
+  app.run(debug=True)
